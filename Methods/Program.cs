72fdb8ca68da94c73d130d263c253bb2128a8dad
ezeleke <abefikru@gmail.com>
2022-08-08 08:24:21 -0700

    methods first commit

---
+++ Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" What is your name?");
+            var name = Console.ReadLine ();
+             Console.WriteLine (name);
+            
+        }
+    }
+}
